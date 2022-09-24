89123ad113d9e7c8b9de2f7df5e01ec81120d86b
gordon <gordon@hypernumbers.com>
2022-09-24 17:59:49 +0100

    got vocaloid reading the zip files and stuff

--- vocaloid/lib/args.ex
+++ vocaloid/lib/args.ex
@@ -1,6 +1,9 @@
 defmodule Vocaloid.Args do
    defstruct [
-      file:     :nil
+      file:       :nil,
+      transforms: [],
+      dryrun:     false,
+      name:       :first
       ]
 
     def parse_args(args) do
@@ -8,11 +11,23 @@ defmodule Vocaloid.Args do
       parse_args(args, acc)
    end
 
-   defp parse_args([],                args), do: validate(args)
-   defp parse_args(["-f",     f | t], args), do: parse_args(t, %Vocaloid.Args{args | file:       f})
-   defp parse_args(["--file", f | t], args), do: parse_args(t, %Vocaloid.Args{args | file:       f})
+   defp parse_args([],                       args), do: validate(args)
+   defp parse_args(["-d"               | t], args), do: parse_args(t, %Vocaloid.Args{args | dryrun:     true})
+   defp parse_args(["-dryrun"          | t], args), do: parse_args(t, %Vocaloid.Args{args | dryrun:     true})
+   defp parse_args(["-f",           f  | t], args), do: parse_args(t, %Vocaloid.Args{args | file:       f})
+   defp parse_args(["--file",       f  | t], args), do: parse_args(t, %Vocaloid.Args{args | file:       f})
+   defp parse_args(["-t",           ts | t], args), do: parse_args(t, %Vocaloid.Args{args | transforms: ts})
+   defp parse_args(["--transforms", ts | t], args), do: parse_args(t, %Vocaloid.Args{args | transforms: ts})
+   defp parse_args(["-n",           n  | t], args), do: parse_args(t, %Vocaloid.Args{args | name: n})
+   defp parse_args(["--name",       n  | t], args), do: parse_args(t, %Vocaloid.Args{args | name: n})
 
    defp validate(args) do
-      args
+      %{file: f, transforms: ts} = args
+      case {f, ts} do
+         {nil, _}  -> {:error, "transforms ok, but no file"}
+         {_,   []} -> {:error, "file ok, but no transforms"}
+         {nil, []} -> {:error, "neither file nor transforms"}
+         {_,   _}  -> {:ok, args}
+      end
    end
 end
--- vocaloid/lib/vocaloid.ex
+++ vocaloid/lib/vocaloid.ex
@@ -2,8 +2,12 @@ defmodule Vocaloid.CLI do
 
   alias Vocaloid.Args
 
+  @tempdir ".vocaloid_generator"
+
   @moduledoc """
   Documentation for the `Vocaloid` vpr file transformer.
+
+  60 is Vocaloid midi note for C3
   """
 
   @doc """
@@ -11,13 +15,60 @@ defmodule Vocaloid.CLI do
   """
   def main(args) do
     IO.puts("Vocaloid escript runing")
-    parsed = Args.parse_args(args)
-    {:ok, ziphandle} = :zip.zip_open(String.to_charlist(parsed.file), [:memory])
-    {:ok, [{_filename, contents}]} = :zip.zip_get(ziphandle)
-    :ok = :zip.zip_close(ziphandle)
-    json = Jason.decode(contents)
-    IO.inspect(json, label: "json")
+    #IO.inspect(UUID.uuid1(), label: "uuid")
+    case Args.parse_args(args) do
+      {:error, errors} ->
+        IO.inspect(errors, label: "invalid arguments")
+      {:ok, parsed} ->
+        case parsed.dryrun do
+          true ->
+            IO.inspect(parsed, label: "parsed")
+          false ->
+            process_file(String.to_charlist(parsed.file))
+          end
+      end
   end
 
+  defp process_file(file) do
+    _dir  = Path.dirname(file)
+    ext  = Path.extname(file)
+    _base = Path.basename(file, ext)
+    # the Erlang module `zip` expects a `.zip` file extension
+    # make it so with a temporary file
+
+    _transformed = case ext do
+      ".zip" -> transform(file)
+      _      -> transform_temp_file(file)
+    end
+  end
+
+  defp transform_temp_file(file) do
+    dir  = Path.dirname(file)
+    ext  = Path.extname(file)
+    base = Path.basename(file, ext)
+    tmpdir = Path.join(dir, @tempdir)
+    :ok = File.mkdir_p(tmpdir)
+    zipfile = Path.join([tmpdir, base <> ".zip"])
+    :ok = File.cp(file, zipfile)
+    transform(String.to_charlist(zipfile))
+  end
+
+  defp transform(file) do
+    IO.inspect(file, label: "in transform")
+    {:ok, ziphandle} = :zip.zip_open(file, [:memory])
+    {:ok, [{_filename, contents}]} = :zip.zip_get(ziphandle)
+    :ok = :zip.zip_close(ziphandle)
+    {:ok, json} = Jason.decode(contents)
+    %{"tracks" => tracks} = json
+    for t <- tracks, do: parse_parts(t["parts"])
+  end
+
+  defp parse_parts(parts) do
+    for p <- parts, do: process_notes(p["notes"])
+  end
+
+  defp process_notes(notes) do
+    for n <- notes, do: IO.inspect({n["number"], n["phoneme"]}, label: "note number")
+  end
 
 end
