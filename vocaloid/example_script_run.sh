669fdbe5e06b0010377ec765757f8be9855cc868
gordon <gordon@hypernumbers.com>
2022-09-25 11:38:56 +0100

    tidy ups

--- vocaloid/example_script_run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# this will have the same effect if you drop the --name field
-./vocaloid -f ./priv/13_notes.vpr -t ./priv/transform.midi -n "1 VOCALOID"
---
+++ vocaloid/test_script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# this will have the same effect if you drop the --name field
+./vocaloid -f ./priv/13_notes.vpr -t ./priv/testing.transpose -n "1 VOCALOID"
